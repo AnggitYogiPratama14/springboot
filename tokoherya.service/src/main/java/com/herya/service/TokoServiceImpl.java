4431db4c67e4621142fdcca933636ce939d58f46
alfanherya <alfanherya@gmail.com>
2020-09-02 17:36:40 +0700

    [feature-tokoHerya]lesson 1 correction #1

--- tokoherya.service/src/main/java/com/herya/service/TokoServiceImpl.java
+++ tokoherya.service/src/main/java/com/herya/service/TokoServiceImpl.java
@@ -1,9 +1,10 @@
 package com.herya.service;
 
 import com.herya.model.TokoDto;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
+@Service
 public class TokoServiceImpl implements TokoService {
     @Override
     public TokoDto getTokoById(UUID tokoId){
